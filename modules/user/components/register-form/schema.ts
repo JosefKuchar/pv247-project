1fb6a6263fb6618d1d97272ebbfd54f5030f4e38
Stefan Peknik <stfn.peknik@gmail.com>
2025-11-29 19:47:56 +0100

    feat(): zod register schema def with stronger constraints

--- modules/user/components/register-form/schema.ts
+++ modules/user/components/register-form/schema.ts
@@ -1,11 +1,23 @@
 import { z } from 'zod';
 
-export const registerFormSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  handle: z.string().min(1, 'Handle is required'),
-  email: z.string().min(1, 'Email is required'),
-  password: z.string().min(1, 'Password is required'),
-  image: z.string().optional(),
-});
+// TODO: proper filtering of valid strings
+export const registerFormSchema = z
+  .object({
+    name: z.string().min(1, 'Name is required'),
+    handle: z.string().min(1, 'Handle is required'),
+    email: z.email('Email is required'),
+    password: z.string().min(8, 'Password is required'),
+    confirmPassword: z.string().min(8, 'Please confirm your password'),
+    image: z.mime(['image/jpeg', 'image/png', 'image/jpg']),
+  })
+  .superRefine((val, ctx) => {
+    if (val.password !== val.confirmPassword) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Passwords are not the same',
+        path: ['confirmPassword'],
+      });
+    }
+  });
 
 export type RegisterFormSchema = z.infer<typeof registerFormSchema>;
