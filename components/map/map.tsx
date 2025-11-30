1b5256edb2a1cde82bc9492455e0083315a546a8
JiriStipek <567776@muni.cz>
2025-11-30 12:23:30 +0100

    feat: add map

---
+++ components/map/map.tsx
@@ -0,0 +1,54 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useLocations } from '@/hooks/use-locations';
+import { LocationMarker } from './location-marker';
+import { PlaceMarkerIcon } from './place-marker-icon';
+
+export function Map() {
+  const locations = useLocations();
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        overflow: 'hidden',
+      }}
+    >
+      <MapContainer
+        center={{ lat: 40.7589, lng: -73.9851 }}
+        zoom={15}
+        scrollWheelZoom={true}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {locations.map(loc => (
+          <Marker
+            key={loc.id}
+            position={{ lat: loc.latitude, lng: loc.longitude }}
+            icon={PlaceMarkerIcon}
+          >
+            <Popup>
+              <strong>{loc.name}</strong>
+              {loc.address && (
+                <>
+                  <br />
+                  {loc.address}
+                </>
+              )}
+            </Popup>
+          </Marker>
+        ))}
+
+        <LocationMarker />
+      </MapContainer>
+    </div>
+  );
+}
