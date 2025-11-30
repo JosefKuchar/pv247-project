3433ef408a45ba4f3926ede43d33a9e2bee757f6
JiriStipek <567776@muni.cz>
2025-11-30 12:22:49 +0100

    feat: custom hooks for locations

---
+++ hooks/use-locations.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+export interface Location {
+  id: string;
+  name: string;
+  address: string | null;
+  latitude: number;
+  longitude: number;
+}
+
+export function useLocations() {
+  const [locations, setLocations] = useState<Location[]>([]);
+
+  useEffect(() => {
+    fetch('/api/locations')
+      .then(res => res.json())
+      .then(setLocations)
+      .catch(err => console.error('Error fetching locations:', err));
+  }, []);
+
+  return locations;
+}
