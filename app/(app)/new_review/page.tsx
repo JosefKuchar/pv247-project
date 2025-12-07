aed625cda31058710b36f1b636595ebe24ebb6d0
Josef Kuchař <josef.kuchar267@gmail.com>
2025-12-07 22:40:23 +0100

    feat: make review creation work somehow, will be refactored later

--- app/(app)/new_review/page.tsx
+++ app/(app)/new_review/page.tsx
@@ -9,6 +9,7 @@ import {
   FormTextarea,
 } from '@/components/form';
 import { Button } from '@/components/ui/button';
+import { createReview } from '@/modules/review/action';
 import { FormProvider, useForm } from 'react-hook-form';
 
 export default function Page() {
@@ -20,12 +21,11 @@ export default function Page() {
         <form
           onSubmit={methods.handleSubmit(async () => {
             const formData = new FormData();
-            formData.append('locationId', methods.getValues('locationId'));
             formData.append('description', methods.getValues('description'));
             formData.append('rating', methods.getValues('rating'));
             formData.append('locationId', methods.getValues('location'));
             // formData.append('photos', methods.getValues('photos'));
-            // await createReview(formData);
+            await createReview(formData);
           })}
         >
           <FormCombobox
