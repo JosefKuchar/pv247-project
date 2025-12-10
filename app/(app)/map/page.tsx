9313f63cb4a98066cb7ab0ce4b1476794836b960
Josef Kuchař <josef.kuchar267@gmail.com>
2025-12-10 16:47:03 +0100

    hotfix: map has to be dynamic

--- app/(app)/map/page.tsx
+++ app/(app)/map/page.tsx
@@ -1,6 +1,8 @@
 import { Map } from '@/components/map/map';
 import { getAllLocations } from '@/modules/location/server';
 
+export const dynamic = 'force-dynamic';
+
 const Page = async () => {
   const locations = await getAllLocations();
 
